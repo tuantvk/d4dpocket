b5ca4127111d23aee9c6dbd2d684dbfc8a4d62d1
tuantv <vantuan@lubrytics.com>
2022-09-30 14:05:20 +0700

    fix: check folder hooks exits

--- src/scripts/usePaging.sh
+++ src/scripts/usePaging.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 _pwd=`pwd`
 
-cp ./usePaging.js "$_pwd/src/hooks/usePaging.js"
+if [ ! -d "$_pwd/src/hooks" ]; then
+  mkdir -p "$_pwd/src/hooks"
+fi
 
+cp "$_pwd/node_modules/d4dpocket/src/scripts/usePaging.js" "$_pwd/src/hooks/usePaging.js"
 echo "> Done!!!"
